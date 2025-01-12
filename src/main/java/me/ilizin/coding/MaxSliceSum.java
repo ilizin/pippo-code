0594c9dd4e0bddeb1bb0b83fc34f71ef1e89661c
ilizin <ilio.zincone@gmx.com>
2025-01-12 18:13:44 +0100

    progressing implementing the max slise sum problem

--- src/main/java/me/ilizin/coding/MaxSliceSum.java
+++ src/main/java/me/ilizin/coding/MaxSliceSum.java
@@ -2,8 +2,32 @@ package me.ilizin.coding;
 
 public class MaxSliceSum {
 
-    public int findMaxSliceSum(int[] a) {
-        return 0;
+    public int findMaxSliceSum(int[] values) {
+        int qIndex = 0;
+        int sliceSum = 0;
+        int maxSliceSum = values[0];
+        int pIndex = 0;
+        for (int i = 0; i < values.length; i++) {
+            sliceSum += values[i];
+            if (sliceSum > maxSliceSum) {
+                maxSliceSum = sliceSum;
+                qIndex = i;
+            }
+            if(pIndex < i && sliceSum - values[pIndex] > sliceSum) {
+                sliceSum = sliceSum - values[pIndex];
+                pIndex++;
+                maxSliceSum = sliceSum;
+            }
+        }
+
+        for (int i = pIndex; i < qIndex; i++) {
+            sliceSum -= values[i];
+            if (sliceSum > maxSliceSum) {
+                maxSliceSum = sliceSum;
+            }
+        }
+
+        return maxSliceSum;
     }
 
 }
---
+++ src/test/java/me/ilizin/coding/MaxSliceSumTest.java
@@ -0,0 +1,37 @@
+package me.ilizin.coding;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class MaxSliceSumTest {
+
+    @Parameterized.Parameters(name = "{index}: values = {0}, expectedMaxSliceSum = {1}")
+    public static Object[][] primeNumbers() {
+        return new Object[][] {
+                { new int[] { 3, 2, -6, 4, 0 }, 5},
+                { new int[] { -1, -1, -1, -1, -1 }, -1},
+                { new int[] { 0, -1, -1, -1, -1 }, 0},
+                { new int[] { -1, -1, -1, -1, 10 }, 10},
+                { new int[] { -3, -1, -1 }, -1},
+                { new int[] { -5, -5, -5 , -5, 5, 5, -5, -5, -5, 5, -5}, 10},
+        };
+    }
+
+    private final int[] values;
+    private final int expectedMaxSliceSum;
+
+    public MaxSliceSumTest(int[] values, int expectedMaxSliceSum) {
+        this.values = values;
+        this.expectedMaxSliceSum = expectedMaxSliceSum;
+    }
+
+    @Test
+    public void findTheSmallestPositiveMissingInteger () {
+        MaxSliceSum maxSliceSum = new MaxSliceSum();
+        Assert.assertEquals(expectedMaxSliceSum, maxSliceSum.findMaxSliceSum(values));
+    }
+
+}
