6d9ed30267050b39ab6d5a6faf210ee18634e255
ilizin <ilio.zincone@gmx.com>
2025-10-05 09:59:10 +0200

    Improve comment

--- src/main/java/me/ilizin/coding/BinaryGap.java
+++ src/main/java/me/ilizin/coding/BinaryGap.java
@@ -10,14 +10,19 @@ public class BinaryGap {
     public int findTheLongestBinaryGap(int value) {
         /* We stop the loop if the following is false:
            1) value != 0
-                If value is 0 we reached the most significant bit (the leftmost one) */
+                If value is 0 we reached the most significant bit (the leftmost one) which means we've validated all the bits. */
         return findTheLongestBinaryGapCommon(value, (param1, param2, parma3) -> param1 != 0);
     }
 
     public int findTheLongestBinaryGapWithFasterExit(int value) {
         /* We stop the loop if one of the following is false:
            1) value != 0
-           2) maxBinaryGapLength < binaryLength + currentBinaryGapLength - 1 */
+           2) maxBinaryGapLength < binaryLength + currentBinaryGapLength - 1
+                a. The binaryLength is the value number of bits.
+                b. The currentBinaryGapLength is the current binary gap length counter.
+                If the maximum binary gap length is less than binaryLength + currentBinaryGapLength it means that there's no
+                chance the currentBinaryGapLength is turned into the new maximum.
+                Why do we rest one ? It's to take in account the rightmost bit 1 we need to close the current binary gap. */
         return findTheLongestBinaryGapCommon(value, (param1, param2, parma3) -> param1 != 0 && (param2 < parma3));
     }
 
