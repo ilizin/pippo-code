850144807638c051b15f6f72c41f04193f591e73
ilizin <ilio.zincone@gmx.com>
2025-10-14 23:33:48 +0200

    Add junit

---
+++ src/test/java/me/ilizin/coding/MaxDoubleSliceSumTest.java
@@ -0,0 +1,31 @@
+package me.ilizin.coding;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class MaxDoubleSliceSumTest {
+
+    @Parameterized.Parameters(name = "{index}: numberOfCounters = {0}, operations = {3}")
+    public static Object[][] provideTestData() {
+        return new Object[][] {
+                { new int[] {3, 2, 6, -1, 4, 5, -1, 2}, 17, "[3, 2, 6, -1, 4, 5, -1, 2]" }
+        };
+    }
+
+    private final int[] values;
+    private final int maximalDoubleSliceSum;
+
+    public MaxDoubleSliceSumTest(int[] values, int maximalDoubleSliceSum, String valuesString) {
+        this.values = values;
+        this.maximalDoubleSliceSum = maximalDoubleSliceSum;
+    }
+
+    @Test
+    public void findMaxDoubleSliceSum () {
+        MaxDoubleSliceSum maxDoubleSliceSum = new MaxDoubleSliceSum();
+        Assert.assertEquals(maximalDoubleSliceSum, maxDoubleSliceSum.findMaxDoubleSliceSum(values));
+    }
+}
