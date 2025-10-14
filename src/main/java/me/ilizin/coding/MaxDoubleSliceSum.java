a20b956a999342adc4f10ce0a2c21903659741e8
ilizin <ilio.zincone@gmx.com>
2025-10-14 23:23:19 +0200

    Start MaxDoubleSliceSum

--- src/main/java/me/ilizin/coding/MaxDoubleSliceSum.java
+++ src/main/java/me/ilizin/coding/MaxDoubleSliceSum.java
@@ -3,6 +3,18 @@ package me.ilizin.coding;
 public class MaxDoubleSliceSum {
 
     public int findMaxDoubleSliceSum(int[] a) {
+        int i = 0;
+        int j = 1;
+        int k = 2;
+
+        int max = 0;
+        int sum = 0;
+        for (k = 3; k < a.length; k++) {
+            sum += a[k - 1];
+            if (sum > max) {
+                max = sum;
+            }
+        }
         return 0;
     }
 
