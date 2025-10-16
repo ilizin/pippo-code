1c746f543e69ec65b480e47fbd64cfc0381a8711
ilizin <ilio.zincone@gmx.com>
2025-10-16 21:54:14 +0200

    first wrong MaxDoubleSliceSum solution

--- src/main/java/me/ilizin/coding/MaxDoubleSliceSum.java
+++ src/main/java/me/ilizin/coding/MaxDoubleSliceSum.java
@@ -3,19 +3,29 @@ package me.ilizin.coding;
 public class MaxDoubleSliceSum {
 
     public int findMaxDoubleSliceSum(int[] a) {
-        int i = 0;
-        int j = 1;
-        int k = 2;
 
         int max = 0;
         int sum = 0;
-        for (k = 3; k < a.length; k++) {
+        for (int k = 3; k < a.length; k++) {
             sum += a[k - 1];
             if (sum > max) {
                 max = sum;
             }
         }
-        return 0;
+        for (int j = 2; j < a.length - 1; j++) {
+            sum += a[j - 1];
+            sum -= a[j];
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        for (int i = 1; i < a.length - 2; i++) {
+            sum -= a[i];
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        return max;
     }
 
 }
