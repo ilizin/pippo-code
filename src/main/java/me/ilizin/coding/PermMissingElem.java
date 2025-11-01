6603e11c0b6b0d0237ca748dc19f6170d56d614d
ilizin <ilio.zincone@gmx.com>
2025-11-01 19:01:43 +0100

    Another solution for PermMissingElem

--- src/main/java/me/ilizin/coding/PermMissingElem.java
+++ src/main/java/me/ilizin/coding/PermMissingElem.java
@@ -1,7 +1,10 @@
 package me.ilizin.coding;
 
+import me.ilizin.coding.annotations.Silly;
+
 public class PermMissingElem {
 
+    @Silly
     public int findMissingElement(int[] values) {
         int[] readValues = new int[values.length + 1];
         for (int value: values) {
@@ -11,6 +14,28 @@ public class PermMissingElem {
         boolean found = false;
         while (!found) {
             if (readValues[i] == 0) {
+                found = true;
+            } else {
+                i++;
+            }
+        }
+        return i + 1;
+    }
+
+    public int findMissingElementWithoutExtraArraySupport(int[] values) {
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] != i + 1 && values[i] != values.length + 1) {
+                int swap = values[i];
+                int swap2 = values[values[i] - 1];
+                values[i] = swap2;
+                values[swap - 1] = swap;
+            }
+        }
+
+        int i = 0;
+        boolean found = false;
+        while (!found && i < values.length) {
+            if (values[i] != i + 1) {
                 found = true;
             } else {
                 i++;
--- src/test/java/me/ilizin/coding/PermMissingElemTest.java
+++ src/test/java/me/ilizin/coding/PermMissingElemTest.java
@@ -12,6 +12,19 @@ public class PermMissingElemTest {
     public static Object[][] provideTestData() {
         return new Object[][] {
                 { new int[] {2, 3, 1, 5}, 4, "[2, 3, 1, 5]" },
+                { new int[] {5, 4, 3, 2}, 1, "[5, 4, 3, 2]" },
+                { new int[] {2}, 1, "[2]" },
+                { new int[] {1}, 2, "[1]" },
+                { new int[] {1, 2, 3, 4}, 5, "[1, 2, 3, 4]" },
+                { new int[] {2, 3, 4, 5}, 1, "[2, 3, 4, 5]" },
+                { new int[] {5, 4, 3, 2}, 1, "[5, 4, 3, 2]" },
+                { new int[] {4, 3, 2, 1}, 5, "[4, 3, 2, 1]" },
+                { new int[] {4, 5, 2, 1}, 3, "[4, 5, 2, 1]" },
+                { new int[] {4, 5, 2, 3}, 1, "[4, 5, 2, 3]" },
+                { new int[] {9, 1, 5, 3, 6, 4, 8, 10, 7, 2}, 11,
+                        "[9, 1, 5, 3, 6, 4, 8, 10, 7, 2 ]" },
+                { new int[] {11, 9, 19, 8, 14, 3, 6, 15, 2, 17, 13, 5, 10, 4, 12, 1, 16, 18, 20, 7}, 21,
+                        "[11, 9, 19, 8, 14, 3, 6, 15, 2, 17, 13, 5, 10, 4, 12, 1, 16, 18, 20, 7]" },
         };
     }
 
@@ -27,5 +40,11 @@ public class PermMissingElemTest {
     public void findMissingElement () {
         PermMissingElem permMissingElem = new PermMissingElem();
         Assert.assertEquals(expectedMissingInteger, permMissingElem.findMissingElement(values));
+    }
+
+    @Test
+    public void findMissingElementWithoutExtraArraySupport () {
+        PermMissingElem permMissingElem = new PermMissingElem();
+        Assert.assertEquals(expectedMissingInteger, permMissingElem.findMissingElementWithoutExtraArraySupport(values));
     }
 }
