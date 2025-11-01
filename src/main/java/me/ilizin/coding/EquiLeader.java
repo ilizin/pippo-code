1b0084c44e94ef6b17bece5310809dc4bb51eeb1
ilizin <ilio.zincone@gmx.com>
2025-11-01 11:30:34 +0100

    Simplify solution EquiLEader (2)

--- src/main/java/me/ilizin/coding/EquiLeader.java
+++ src/main/java/me/ilizin/coding/EquiLeader.java
@@ -1,15 +1,15 @@
 package me.ilizin.coding;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class EquiLeader {
 
     public int findNumberOfEquiLeader(int[] values) {
-        if (values.length == 1) {
-            return 0;
-        }
+
         int[] dominatorsLeftToRight = new int[values.length];
+        Arrays.fill(dominatorsLeftToRight, -1);
 
         int dominatorOccurrences = -1;
         int dominator = -1;
@@ -24,31 +24,30 @@ public class EquiLeader {
                 dominator = value;
             } else if (dominatorOccurrences > (i + 1) / 2) {
                 dominatorsLeftToRight[i] = dominator;
-            } else {
-                dominatorsLeftToRight[i] = -1;
             }
         }
 
         dominatorOccurrences = -1;
         dominator = -1;
         int equiLeaderCount = 0;
-        memory = new HashMap<>();
+        memory.clear();
         for (int i = values.length - 1; i >= 1; i--) {
             int value = values[i];
             Integer occurrences = memory.getOrDefault(value, 0);
             memory.put(value, occurrences + 1);
             if (occurrences + 1 > (values.length - i) / 2) {
+                dominatorOccurrences = occurrences + 1;
+                dominator = value;
                 if (dominatorsLeftToRight[i - 1] == value) {
                     equiLeaderCount++;
                 }
-                dominatorOccurrences = occurrences + 1;
-                dominator = value;
             } else if (dominatorOccurrences > (values.length - i) / 2) {
                 if (dominatorsLeftToRight[i - 1] == dominator) {
                     equiLeaderCount++;
                 }
             }
         }
+
         return equiLeaderCount;
     }
 }
