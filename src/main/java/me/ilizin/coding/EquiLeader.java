7c4185d3fba52ff0e0fffea7815358191f2cef7a
ilizin <ilio.zincone@gmx.com>
2025-11-08 12:40:42 +0100

    Use right names for methods EquiLeader

--- src/main/java/me/ilizin/coding/EquiLeader.java
+++ src/main/java/me/ilizin/coding/EquiLeader.java
@@ -12,36 +12,43 @@ public class EquiLeader {
         int[] dominatorsLeftToRight = new int[values.length];
         Arrays.fill(dominatorsLeftToRight, -1);
 
-        getIntegerIntegerMap(values, dominatorsLeftToRight, false, 0,
+        findNumberOfEquiLeaderFromLeftToRight(values, dominatorsLeftToRight);
+        return findNumberOfEquiLeaderFromRightToLeft(values, dominatorsLeftToRight);
+    }
+
+    private int findNumberOfEquiLeaderFromLeftToRight(int[] values,int[] dominatorsLeftToRight) {
+        return findNumberOfEquiLeaderCommon(values, dominatorsLeftToRight, false, 0,
                 i -> i < values.length, i -> i + 1, i -> i);
-        return getIntegerIntegerMap(values, dominatorsLeftToRight, true, values.length - 1,
+    }
+
+    private int findNumberOfEquiLeaderFromRightToLeft(int[] values,int[] dominatorsLeftToRight) {
+        return findNumberOfEquiLeaderCommon(values, dominatorsLeftToRight, true, values.length - 1,
                 i -> i >= 1, i -> i - 1, i ->  (values.length - i) - 1);
     }
 
-    private int getIntegerIntegerMap(int[] values, int[] dominatorsLeftToRight, boolean isRightToLeft,
-                                             int firstValue, Predicate<Integer> a, Function<Integer, Integer> step,
-                                            Function<Integer, Integer> index) {
+    private int findNumberOfEquiLeaderCommon(int[] values, int[] dominatorsLeftToRight, boolean isRightToLeft,
+                                     int firstValue, Predicate<Integer> a, Function<Integer, Integer> step,
+                                     Function<Integer, Integer> index) {
+
         int dominatorOccurrences = -1;
         int dominator = -1;
         int equiLeaderCount = 0;
 
         Map<Integer, Integer> memory = new HashMap<>();
         for (int i = firstValue; a.test(i); i = step.apply(i)) {
-            int indexx = index.apply(i);
 
             int value = values[i];
             int occurrences = memory.getOrDefault(value, 0) + 1;
             memory.put(value, occurrences);
-            if (occurrences  > (indexx + 1) / 2) {
+            if (occurrences  > (index.apply(i) + 1) / 2) {
                 dominatorsLeftToRight[i] = value;
                 dominatorOccurrences = occurrences;
                 dominator = value;
                 if (isRightToLeft && dominatorsLeftToRight[i - 1] == dominatorsLeftToRight[i]) {
                     equiLeaderCount++;
                 }
-            } else if (dominatorOccurrences > (indexx + 1) / 2) {
+            } else if (dominatorOccurrences > (index.apply(i) + 1) / 2) {
                 dominatorsLeftToRight[i] = dominator;
-
                 if (isRightToLeft && dominatorsLeftToRight[i - 1] == dominatorsLeftToRight[i]) {
                     equiLeaderCount++;
                 }
