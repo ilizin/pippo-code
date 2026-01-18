b6197c952de64b343b7621d5e85e34191ab778c3
ilizin <ilio.zincone@gmx.com>
2026-01-18 10:42:59 +0100

    Decrease by one the number of rows MaxDoubleSliceSum problem

--- src/main/java/me/ilizin/coding/MaxDoubleSliceSum.java
+++ src/main/java/me/ilizin/coding/MaxDoubleSliceSum.java
@@ -3,11 +3,11 @@ package me.ilizin.coding;
 public class MaxDoubleSliceSum {
 
     public int findMaxDoubleSliceSum(int[] values) {
-        int[][] maxSliceSum = new int[values.length][values.length];
-        int[] maxs = new int[values.length];
+        int[][] maxSliceSum = new int[values.length - 1][values.length];
+        int[] maxs = new int[values.length - 1];
         int sum = 0;
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < values.length; i++) {
+        for (int i = 0; i < values.length - 1; i++) {
             for (int j = i + 1; j < values.length; j++) {
                 if (j == i + 1) {
                     maxSliceSum[i][j] = 0;
