0dbb9f5ae299c809564690ed2317a2201553aa60
ilizin <ilio.zincone@gmx.com>
2026-01-19 15:57:11 +0100

    Reduce memory space MaxDoubleSliceSum problem

--- src/main/java/me/ilizin/coding/MaxDoubleSliceSum.java
+++ src/main/java/me/ilizin/coding/MaxDoubleSliceSum.java
@@ -3,19 +3,21 @@ package me.ilizin.coding;
 public class MaxDoubleSliceSum {
 
     public int findMaxDoubleSliceSum(int[] values) {
-        int[][] maxSliceSum = new int[values.length - 1][values.length];
+        int[][] maxSliceSum = new int[values.length - 1][];
         int[] maxs = new int[values.length];
         int max = Integer.MIN_VALUE;
 
         for (int i = 0; i < values.length - 1; i++) {
+            maxSliceSum[i] = new int[values.length - i];
+
             for (int j = i + 1; j < values.length; j++) {
                 if (j == i + 1) {
-                    maxSliceSum[i][j] = 0;
+                    maxSliceSum[i][j - i] = 0;
                 } else {
-                    maxSliceSum[i][j] = maxSliceSum[i][j - 1] + values[j - 1];
+                    maxSliceSum[i][j - i] = maxSliceSum[i][(j - i) - 1] + values[j - 1];
                 }
-                if (maxSliceSum[i][j] > max) {
-                   max =  maxSliceSum[i][j];
+                if (maxSliceSum[i][j - i] > max) {
+                   max = maxSliceSum[i][j - i];
                 }
             }
             maxs[i] = max;
@@ -25,8 +27,8 @@ public class MaxDoubleSliceSum {
         int maxDoubleSliceSum = 0;
         for (int i = 0; i < values.length - 1; i++) {
             for (int j = i + 1; j < values.length - 1; j++) {
-                if (maxSliceSum[i][j] + maxs[j] > maxDoubleSliceSum) {
-                    maxDoubleSliceSum = maxSliceSum[i][j] + maxs[j];
+                if (maxSliceSum[i][j - i] + maxs[j] > maxDoubleSliceSum) {
+                    maxDoubleSliceSum = maxSliceSum[i][j - i] + maxs[j];
                 }
             }
         }
